710800cf214760dd7ba353bdfcd64152729af8cd
lbihani9 <lokeshbihani99@gmail.com>
2025-06-30 15:36:18 +0530

    Updated version

--- server/src/main/java/org/jinn/RaftNodeRunner.java
+++ server/src/main/java/org/jinn/RaftNodeRunner.java
@@ -6,19 +6,18 @@ import java.io.IOException;
 
 /**
  * TODO:
- * 1. Snapshots & Checkpointing [Least priority for now]
- * 2. Flushing entries from memory that have already been applied to state machine. ( < commitIndex)
+ * 1. Snapshots & Checkpointing
+ * 2. Flushing entries from memory that have already been replicated to majority followers to keep memory usage minimal. This may be fixed with snapshotting.
  * 3. Optimize RaftLogManager in terms of deletion.
  * 4. Optimize LogEntriesBytes size tracker to initiate flushing.
  * 5. Implement Client side logic to actually call "write" operation on raft cluster and perform replication.
  * 6. Test and optimize pending futures operations for client writes.
- * 7. Dynamic node addition and removal [Lead priority for now]
+ * 7. Dynamic node addition and removal
  * 8. Separate out LogEntries logs and other states
- * 9. Support compaction [Lead priority for now]
- * 10. Optimize reads and writes from/to disk. [Lead priority for now]
- * 11. Reads from raft cluster
+ * 9. Support compaction
+ * 10. Optimize reads and writes from/to disk.
+ * 11. Read from raft cluster
  * 12. Minimalistic interactive terminal to connect with the cluster and perform SET, GET and CONNECT operations.
- *      - SET command is not working.
  * 13. Test for consistency related things.
  *
  *
